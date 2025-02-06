59b50e2ec1931e9cf5069c8fd5c2c1616c620787
ivnzdrs <ivandres.student@ua.edu.ph>
2025-02-06 18:00:24 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Ivan Andres</h1>
+      <div className="box">
+        <p className="text">CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
